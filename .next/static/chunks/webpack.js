81df52ba0d0788e419bfeb2054b18d912e3920d9
hemanthr3 <hemanth@kodnest.com>
2023-02-15 23:41:17 +0530

    home page ui is done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6253a58c127131c545e5"; }
+/******/ 		__webpack_require__.h = function() { return "66d009753d075279d054"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
